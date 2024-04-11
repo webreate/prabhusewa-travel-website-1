899239a5e75342b48f740ddbfe5a6c9713b9b58d
suraj <Surajkumaryadav@webreate.com>
2024-04-11 12:45:55 +0530

    taking pull

--- src/component/gallery/Gallerycontent.jsx
+++ src/component/gallery/Gallerycontent.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-// import "../../style/gallery/Gallerycontent.css";
+import "../../style/gallery/Gallerycontent.css";
 
 const Gallerycontent = () => {
   const [gdata, setGdata] = useState([]);
