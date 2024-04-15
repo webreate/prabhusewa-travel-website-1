661a87ab81454d6418ab03f18fd401b8fd9cc78f
Abhishek kumar <128677325+Akdevit@users.noreply.github.com>
2024-04-15 18:07:17 +0530

    header and api and more work are done

--- src/component/aboutus/Populartour.jsx
+++ src/component/aboutus/Populartour.jsx
@@ -37,23 +37,6 @@
       pauseOnHover: false, // Pause autoplay on hover
       pauseOnFocus: false ,// Pause autoplay on focus
       responsive: [
-        // {
-        //   breakpoint: 1024,
-        //   settings: {
-        //     slidesToShow: 3,
-        //     slidesToScroll: 3,
-        //     infinite: true,
-        //     dots: true
-        //   }
-        // },
-        // {
-        //   breakpoint: 600,
-        //   settings: {
-        //     slidesToShow: 2,
-        //     slidesToScroll: 2,
-        //     initialSlide: 2
-        //   }
-        // },
         {
           breakpoint: 480,
           settings: {
