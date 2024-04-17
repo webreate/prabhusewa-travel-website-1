20211a4cdb3a075873b69647866b46487912f94c
suraj <Surajkumaryadav@webreate.com>
2024-04-17 12:10:11 +0530

    commiting after converting px into percentage

--- src/pages/Packagespage.jsx
+++ src/pages/Packagespage.jsx
@@ -3,14 +3,11 @@ import React from "react";
 import Dodhamyatra from "../component/packages/Dodhamyatra";
 import Header from "../component/layout/Header";
 import Footer from "../component/layout/footer";
-import data from "../component/Shared/HeroSection/HeroData.json";
-import HeroSection from "../component/Shared/HeroSection/HeroSection";
 const Packages = () => {
-  const homeData = data.find((item) => item.id === 7);
   return (
     <>
       <Header />
-      <HeroSection {...homeData} />
+      {/* <Galleryhero/> */}
       <Dodhamyatra />
       <Footer />
     </>
